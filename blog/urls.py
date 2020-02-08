68ff768289794eb99e5db7dc3c29aa55e61ec4c1
hasan29351 <hasan29351@gmail.com.com>
2020-02-08 12:42:27 +0600

    08-02-20

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import BlogListView,BlogDetailView,BlogCreateView,BlogUpdateView,BlogDeleteView
+
+
+urlpatterns = [
+
+	path('', BlogListView.as_view(), name='home'),
+	path('post/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+	path('post/new/', BlogCreateView.as_view(), name='post_new'),
+	path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+	path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+
+
+]
